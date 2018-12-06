08e957b3870cd71537f1f4e87ae2ae2cf8078465
Max Zheng <34936931+54bp6cl6@users.noreply.github.com>
2018-12-06 18:34:58 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -30,10 +30,32 @@ def callback():
         abort(400)
     return 'OK'
 
+def KeyWord(text):
+    KeyWordDict = {"你好":"你也好啊",
+                   "你是誰":"我是大帥哥",
+                   "帥":"帥炸了"}
+    for k in KeyWordDict.keys():
+        if text.find(k) != -1:
+            return [True,KeyWordDict[k]]
+    return [False]
+
+def Reply(event):
+    Ktemp = KeyWord(event.message.text)
+    if Ktemp[0]:
+        line_bot_api.reply_message(event.reply_token,
+            TextSendMessage(text = Ktemp[1]))
+    else:
+        line_bot_api.reply_message(event.reply_token,
+            TextSendMessage(text = event.message.text))
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text = event.message.text))
+    try:
+        Reply(event)
+    except Exception as e:
+        line_bot_api.reply_message(event.reply_token, 
+            TextSendMessage(text=str(e)))
 
 import os
 if __name__ == "__main__":
