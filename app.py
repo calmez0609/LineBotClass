0744478f9d255d88fe8c9c77df098e37f597efeb
Max Zheng <34936931+54bp6cl6@users.noreply.github.com>
2018-12-14 16:54:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def handle_sticker_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         StickerSendMessage(
-            package_id='1'
+            package_id='1',
             sticker_id='410')
     )
 
